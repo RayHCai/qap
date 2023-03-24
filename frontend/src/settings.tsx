c45216566f86d2fec2b5f4e5bd6f9b0d28bf9f4a
Ray Cai Flanagan <hjkareus@gmail.com>
2023-03-24 09:18:18 -0400

    Update settings.tsx

--- frontend/src/settings.tsx
+++ frontend/src/settings.tsx
@@ -1,3 +1,3 @@
 export const DEBUG = process.env.NODE_ENV === 'development';
 
-export const SERVER_URL = DEBUG ? 'http://localhost:8000' : '';
+export const SERVER_URL = DEBUG ? 'http://localhost:8000' : 'http://backend-production-0b66.up.railway.app';
