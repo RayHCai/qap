edd7f4e699a98a539d227ce2f94a735e55cad6ba
Ray Cai Flanagan <hjkareus@gmail.com>
2023-07-13 09:16:25 -0400

    React

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,26 +1,3 @@
-import { useState, useEffect, ReactNode } from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { render } from 'react-dom';
 
-import { SERVER_URL } from './settings';
-
-import './index.css';
-
-function App() {
-    return (
-        <>
-            <h1>Fortnite</h1>
-        </>
-        // <BrowserRouter>
-        //     <Routes>
-
-        //     </Routes>
-        // </BrowserRouter>
-    );
-}
-
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
-
-root.render(<App />);
+render(<>Hello World</>, document.getElementById('root'));
--- frontend/src/settings.ts
+++
@@ -1,5 +0,0 @@
-export const DEBUG = process.env.NODE_ENV === 'development';
-
-export const SERVER_URL = DEBUG
-    ? 'http://localhost:8000'
-    : '';
